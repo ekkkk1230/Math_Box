ff0b5ee166a0367a0a6c22141cd2fe69ef60515a
ekkk1230 <ek10314@naver.com>
2023-05-21 12:16:55 +0900

    Change divide function as premium

--- calculator.py
+++ calculator.py
@@ -1,12 +1,12 @@
 # 기본 계산기
-def dadd(a, b);
+def dadd(a, b):
 	return a+b
 
-def subtract(a, b);
+def subtract(a, b):
 	return a-b
 
-def multiply(a, b);
+def multiply(a, b):
 	return a*b
 
-def divide(a, b);
+def divide_premium(a, b):
 	return a/b
